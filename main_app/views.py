73aabb2a700536d67c4f918b4e4cec297c6e8394
danieljo <hyundanieljo@gmail.com>
2023-03-03 11:34:41 -0500

    added update and delete functionality to cats

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 
 # temporary cats for building templates
@@ -31,3 +32,23 @@ def cats_index(request):
 def cats_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     return render(request, 'cats/detail.html', { 'cat': cat })
+
+class CatCreate(CreateView):
+    model = Cat
+    # the fields attribute is required for a createview. These inform the form
+    fields = '__all__'
+    # we could also have written our fields like this:
+    # fields = ['name', 'breed', 'description', 'age']
+    # we need to add redirects when we make a success
+    # success_url = '/cats/{cat_id}'
+    # or, we could redirect to the index page if we want
+    # success_url = '/cats'
+    
+class CatUpdate(UpdateView):
+    model = Cat
+    # let's use custom fields to diallow renaming the cat
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
