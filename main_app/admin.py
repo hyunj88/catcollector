5384208a03b6874172cb285d88933bf76594d937
danieljo <hyundanieljo@gmail.com>
2023-03-03 17:25:16 -0500

    added links to view temmplates for toys index and detail

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 # import your models here
-from .models import Cat, Feeding
+from .models import Cat, Feeding, Toy
 
 # Register your models here.
 admin.site.register(Cat)
 # Register our new feeding model
 admin.site.register(Feeding)
+admin.site.register(Toy)
--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from .models import Cat
-from .models import Toy
+from .models import Cat, Toy
 from .forms import FeedingForm
 
 # temporary cats for building templates
@@ -36,9 +35,13 @@ def cats_index(request):
 def cats_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
 
+    # first we'll get a list of ids of toys the cat owns
+    id_list = cat.toys.all().values_list('id')
+    # then we'll make a list of the toys the cat does not have
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in=id_list)
     #instantiate FeedingForm to be rendered in the templates
     feeding_form = FeedingForm()
-    return render(request, 'cats/detail.html', { 'cat': cat, 'feeding_form': feeding_form })
+    return render(request, 'cats/detail.html', { 'cat': cat, 'feeding_form': feeding_form, 'toys': toys_cat_doesnt_have })
 
 class CatCreate(CreateView):
     model = Cat
@@ -70,6 +73,14 @@ def add_feeding(request, cat_id):
         new_feeding = form.save(commit=False)
         new_feeding.cat_id = cat_id
         new_feeding.save()
+    return redirect('detail', cat_id=cat_id)
+
+def assoc_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('detail', cat_id=cat_id)
+
+def unassoc_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.remove(toy_id)
     return redirect('detail', cat_id=cat_id)
 
 # ToyList
